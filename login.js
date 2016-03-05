69fefc0c54a22e521e8003bed9492ac87c8b582c
George Roots <groots@Georges-MacBook-Air.local>
2016-03-05 10:27:33 -0500

    created boilerplate and login file

--- login.js
+++ login.js
@@ -10,9 +10,9 @@ import React, {
 } from 'react-native';
 
 class Login extends Component {
-	// constructor(props){
-	// 	super(props);
-	// }
+	constructor(props){
+		super(props);
+	}
 
 	render(){
 		return(
