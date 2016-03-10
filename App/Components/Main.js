383886cce121aeca18b1a335181d704e72b8fa0a
George Roots <groots@Georges-MacBook-Air.local>
2016-03-10 16:45:35 -0500

    added NavigatorIOS to app

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -1,15 +1,30 @@
 'use strict';
-import React, {
+import React from 'react-native';
+let {
 	Component,
 	View,
-	Text
-} from 'react-native';
+	Text,
+	StyleSheet
+} = React;
 
+const styles = StyleSheet.create({
+	mainContent: {
+		flex: 1,
+		padding: 30,
+		marginTop: 65,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: '#fff'
+	},
+	whiteText: {
+		color: '#fff'
+	}
+});
 class Main extends Component {
 	render(){
 		return (
-			<View>
-				The making of a homepage...
+			<View style={styles.mainContent}>
+				<Text style={styles.whiteText}>The making of a homepage...</Text>
 			</View>
 
 		);
--- index.ios.js
+++ index.ios.js
@@ -3,45 +3,41 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, {
+
+import React from 'react-native';
+import Main from './App/Components/Main';
+
+let {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
   View,
   NavigatorIOS
-} from 'react-native';
+} = React;
 
-
-class react_mobile_base extends Component {
-  render() {
-    return (
-      <NavigatorIOS
-        initialRoute={{
-          title:'IOS Base App',
-          coponent: Main
-        }} />
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#000fff',
+  }
 });
+
+class react_mobile_base extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+      
+        initialRoute={{
+          title:'IOS Base App',
+          component: Main
+        }} />
+    );
+  }
+}
+
 
 AppRegistry.registerComponent('react_mobile_base', () => react_mobile_base);
